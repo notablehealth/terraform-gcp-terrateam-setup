d5aa90a50391bdaa7459b4caa6d5eb11e4ee4f53
Steven Nemetz <snemetz@notablehealth.com>
2024-07-22 09:25:28 -0700

    Add service account email

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "workload_identity_pool_provider_name" {
-  value = google_iam_workload_identity_pool_provider.terrateam_provider.name
+  description = "Terrateam provider workload identity pool"
+  value       = google_iam_workload_identity_pool_provider.terrateam_provider.name
+}
+output "service_account_email" {
+  description = "Terrateam service account email"
+  value       = google_service_account.terrateam.email
 }
