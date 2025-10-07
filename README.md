c56491f18a31f0d400b04517a3d2c2e98773c478
Brandon Boudrias <brandon.boudrias@notablehealth.com>
2025-10-07 11:27:50 -0700

    refactor to create multiple service accounts per account

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Terrateam GCP OIDC setup
+
+This bootstraps a workload identity pool for github actions and creates service accounts that the pool can access, with optional restrictions to repositories for all, or per service account.
+
+The service accounts' permissions to modify GCP resources have to be granted elsewhere.
--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "google_service_account" "terrateam" {
-  account_id   = var.service_account_name
-  display_name = var.service_account_name
-  description  = var.service_account_description
-  project      = var.project_id
-}
-
 resource "google_iam_workload_identity_pool" "terrateam_pool" {
   project                   = var.project_id
   workload_identity_pool_id = var.workload_identity_pool_id
@@ -24,35 +17,29 @@ resource "google_iam_workload_identity_pool_provider" "terrateam_provider" {
     "attribute.repository_owner" = "assertion.repository_owner"
   }
   oidc {
-    #allowed_audiences = []
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
 }
 locals {
-  service_account_members = length(var.repositories) > 0 ? [
-    for repo in var.repositories : "principalSet://iam.googleapis.com/projects/${data.google_project.current.number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.repository/${var.github_org}/${repo}"
-    ] : [
-    "principalSet://iam.googleapis.com/projects/${data.google_project.current.number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.repository_owner/${var.github_org}"
-  ]
-}
-resource "google_service_account_iam_binding" "terrateam_workload_identity_user" {
-  #resource "google_service_account_iam_member" "terrateam_workload_identity_user" {
-  service_account_id = google_service_account.terrateam.id
-  role               = "roles/iam.workloadIdentityUser"
-  members = local.service_account_members
+  service_account_repositories = { for svc_acc in var.service_accounts :
+    svc_acc.name => length(svc_acc.repositories) > 0 ?
+    [for repo in svc_acc.repositories : "principalSet://iam.googleapis.com/projects/${data.google_project.current.number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.repository/${var.github_org}/${repo}"]
+    :
+    [for repo in var.default_repositories : "principalSet://iam.googleapis.com/projects/${data.google_project.current.number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.repository/${var.github_org}/${repo}"]
+  }
 }
 
-resource "google_project_iam_member" "terrateam_editor" {
-  for_each = { for role in var.service_account_roles : role.role => role }
-  project  = var.project_id
-  role     = each.value.role
-  member   = "serviceAccount:${google_service_account.terrateam.email}"
-  dynamic "condition" {
-    for_each = each.value.condition != null ? [each.value.condition] : []
-    content {
-      title       = condition.value.title
-      description = condition.value.description
-      expression  = condition.value.expression
-    }
-  }
+resource "google_service_account" "terrateam" {
+  for_each     = { for svc_acc in var.service_accounts : svc_acc.name => svc_acc }
+  account_id   = each.key
+  display_name = each.key
+  description  = each.value.description
+  project      = var.project_id
+}
+
+resource "google_service_account_iam_binding" "terrateam_workload_identity_user" {
+  for_each           = { for svc_acc in var.service_accounts : svc_acc.name => svc_acc }
+  service_account_id = google_service_account.terrateam[each.key].id
+  role               = "roles/iam.workloadIdentityUser"
+  members            = local.service_account_repositories[each.key]
 }
